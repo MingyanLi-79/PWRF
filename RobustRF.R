aad680e28f2a64e8488c74e2a4881090c90b9866
MingyanLi-79 <160198949+MingyanLi-79@users.noreply.github.com>
2024-02-15 21:48:32 -0500

    Update RobustRF.R

--- RobustRF.R
+++ RobustRF.R
@@ -4,19 +4,10 @@ RobustRF <- function(OOBWeights, PredWeights, TRAINY, alpha=6, lambda = 2, metho
     Res <- Compute_deltas(OOBWeights=OOBWeights, TRAINY=TRAINY, alpha=alpha, method=method, tol=tol)
   if (method=="PWLS")
     Res <- Compute_deltas_PWLS(OOBWeights=OOBWeights, TRAINY=TRAINY, lambda=lambda, method=method, tol=tol)
-  
-  #adjust weights for training cases
   d <- Res[[1]]
   niter <- Res[[3]]
-  #Apply adjustment to test cases
-  ## method 1 for PWRF
-  # if (method=="PWLS"){
-  #   AdjWts <- d*PredWeights
-  # } else {
-  ## for all other methods
-    D <- matrix(rep(d, nrow(PredWeights)), nrow=nrow(PredWeights), ncol=length(TRAINY), byrow=T)
-    AdjWts <- D*PredWeights
-  # }
+  D <- matrix(rep(d, nrow(PredWeights)), nrow=nrow(PredWeights), ncol=length(TRAINY), byrow=T)
+  AdjWts <- D*PredWeights
   AdjWts <- AdjWts/rowSums(AdjWts)
   Pred <- as.matrix(AdjWts)%*%TRAINY
   return(list(prediction=Pred, adjustedweight=AdjWts, iteration=niter,trainingweight=d))
